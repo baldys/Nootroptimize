f5c09657f0874ec55977616ba47f5906bdb7db65
Veronica Baldys <baldys@icloud.com>
2016-01-27 02:43:40 -0800

    Initial Commit

---
+++ Nootroptimize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nootroptimize
+//
+//  Created by Veronica Baldys on 2016-01-27.
+//  Copyright © 2016 Veronica Baldys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
