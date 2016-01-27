2d98b7e032f30f53806a599c0918c4490f27a359
Veronica Baldys <baldys@icloud.com>
2016-01-27 09:50:10 -0800

    Added stack model

---
+++ Nootroptimize/Stack.swift
@@ -0,0 +1,24 @@
+//
+//  Stack.swift
+//  Nootroptimize
+//
+//  Created by Veronica Baldys on 2016-01-27.
+//  Copyright © 2016 Veronica Baldys. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Stack: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+    class func createInManagedObjectContext(moc: NSManagedObjectContext, name: String) -> Stack {
+        let newItem = NSEntityDescription.insertNewObjectForEntityForName("Stack", inManagedObjectContext: moc) as! Stack
+        newItem.name = name
+        //newItem.itemText = text
+        
+        return newItem
+    }
+
+}
